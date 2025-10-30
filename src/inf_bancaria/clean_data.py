acff0ba6038c39fdd6b7e4e44d3d4c94e994fe88
Elliot Duran <elliotd70@outlook.com>
2025-10-30 14:37:26 -0600

    Tratamiento de los datos

--- src/inf_bancaria/clean_data.py
+++ src/inf_bancaria/clean_data.py
@@ -1,4 +1,4 @@
-def remove_columns(df):
+def remove_variables(df):
     """
     Processes the data frame removing 
     unnecessary columns for the project.
@@ -9,6 +9,6 @@ def remove_columns(df):
        'SEGMENTO_CLIENTE', 'CLIENTE_CDE', 'SUMA_LINEAS_REVOLVENTES', 
        'CAPACIDAD_PAGO_TOTAL','PEOR_MOP_TARJETAS', 'SUMA_SALDOS_TARJETAS', 
        'SUMA_PAGO_MIN_TARJETAS', 'PEOR_HISTORIA_CREDITO', 'SUMA_SALDOS_TOTAL', 
-       'NUM_CREDITOS',]
+       'NUM_CREDITOS',"MESES_VENCIDOS"]
     
     return df.drop(columns, axis=1)
--- src/inf_bancaria/config.py
+++ src/inf_bancaria/config.py
@@ -2,3 +2,6 @@ from pathlib import Path
 
 data_dir = Path('../data/raw')
 data_path = data_dir / "Inf_Bancaria_Base_datos.csv"
+
+clean_data_dir = Path("../data/clean")
+clean_data_path = clean_data_dir / "clean_df.csv"
